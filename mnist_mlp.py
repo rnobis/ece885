4a767811ac5d3ae36c287b227998dc1c8f90ce66
Ryan Nobis <rcnobis@gmail.com>
2017-02-14 21:03:16 -0500

    Added additional layer with 200 outputs, and reduced epochs to 10.

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -18,7 +18,7 @@ from keras.utils import np_utils
 
 batch_size = 128
 nb_classes = 10
-nb_epoch = 20
+nb_epoch = 10
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -41,6 +41,9 @@ model.add(Dense(512, input_shape=(784,)))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))
 model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(200))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))
 model.add(Dense(10))
