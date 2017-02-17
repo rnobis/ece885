2c0fe13265fbe80aa70205444c7e1603ec4e4e5d
Ryan Nobis <rcnobis@gmail.com>
2017-02-17 15:17:05 -0500

    Updated part3 to handle decay with loss function.

--- MP1_Part3.py
+++ MP1_Part3.py
@@ -6,6 +6,7 @@ results.
 
 from __future__ import print_function
 import numpy as np
+import keras.callbacks as cb
 np.random.seed(1337)  # for reproducibility
 
 from keras.datasets import mnist
@@ -13,11 +14,32 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import SGD
 from keras.utils import np_utils
+from keras.callbacks import LearningRateScheduler
 
 
 batch_size = 32 #128
 nb_classes = 10
 nb_epoch = 20
+
+sd=[]
+class LossHistory(cb.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = [1,1]
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        sd.append(step_decay(len(self.losses)))
+        print('learning rate:', step_decay(len(self.losses)))
+        print('derivative of loss:', 2*np.sqrt((self.losses[-1])))
+
+def step_decay(losses):
+    if float((np.array(temp_history.losses[-1])))<nb_epoch:
+        lrate=0.060*np.exp(np.array(temp_history.losses[-1]))
+        return lrate
+    else:
+        lrate=0.01
+        return lrate
+
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -52,17 +74,18 @@ model.add(Activation('softmax'))
 
 model.summary()
 
-learning_rate = 0.1
-decay_rate = learning_rate / nb_epoch
-
-sgd = SGD(lr=learning_rate, decay=decay_rate)
+sgd = SGD(lr=0.0)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
+temp_history = LossHistory()
+lrate = LearningRateScheduler(step_decay)
+callbacks_list = [lrate, temp_history]
+
 history = model.fit(X_train, Y_train,
                     batch_size=batch_size, nb_epoch=nb_epoch,
-                    verbose=1, validation_data=(X_test, Y_test))
+                    verbose=1, validation_data=(X_test, Y_test), callbacks=callbacks_list)
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
