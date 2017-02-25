dc3ce9be8fd7a5c238b1c597f1286f3a79686ddd
Ryan Nobis <rcnobis@gmail.com>
2017-02-25 15:33:20 -0500

    Added files for Mini-Project 2. Part1 needs to be tested on the HPCC.

---
+++ MP2_Part3_RNobis.py
@@ -0,0 +1,66 @@
+'''ECE885 Mini-Project 2 Part 3
+Trains a LSTM RNN on the MNIST dataset.
+
+Updated MP2_Part1_RNobis.py to have a variable learning rate based on the
+loss function, to see the results.
+'''
+
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.utils import np_utils
+
+
+batch_size = 32 #128
+nb_classes = 10
+nb_epoch = 20
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+#Reshape input data to 784 = 28 * 28 (image data is 28x28)
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+#Build neural network model. 
+model = Sequential()
+model.add(Dense(800, input_shape=(784,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(400))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.summary()
+
+sgd = SGD(lr=0.1, decay=0.0)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+history = model.fit(X_train, Y_train,
+                    batch_size=batch_size, nb_epoch=nb_epoch,
+                    verbose=1, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
