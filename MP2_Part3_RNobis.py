201f7699162b9fe75a24410d073dc00d1e8ba0e8
Ryan Nobis <rcnobis@gmail.com>
2017-02-26 14:52:17 -0500

    Changed number of epochs in MP2_Part2 to 100. Updated MP2_Part3 to have a variable learning rate.

--- MP2_Part3_RNobis.py
+++ MP2_Part3_RNobis.py
@@ -1,35 +1,48 @@
 '''ECE885 Mini-Project 2 Part 3
 Trains a LSTM RNN on the MNIST dataset.
 
-Updated MP2_Part1_RNobis.py to have a variable learning rate based on the
+Updated MP2_Part2_RNobis.py to have a variable learning rate based on the
 loss function, to see the results.
 '''
 
 from __future__ import print_function
 import numpy as np
+import keras.callbacks as cb
 np.random.seed(1337)  # for reproducibility
 
 from keras.datasets import mnist
+#from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
+from keras.layers import LSTM
 from keras.utils import np_utils
-
+from keras.callbacks import LearningRateScheduler
 
 batch_size = 32 #128
 nb_classes = 10
-nb_epoch = 20
+nb_epoch = 100
+
+sd=[]
+class LossHistory(cb.Callback):
+    def on_train_begin(self, logs={}):
+        #Create array for losses
+        self.losses = [1,1]
+
+    def on_epoch_end(self, batch, logs={}):
+        #Populate arrays for losses and step decays, print out learning rate
+        self.losses.append(logs.get('loss'))
+        sd.append(step_decay(len(self.losses)))
+        print('\nlearning rate:', step_decay(len(self.losses)))
+
+def step_decay(losses):
+    #Calculate learning rate based on loss function
+    lrate=0.075*np.exp(np.array(temp_history.losses[-1]))
+    return lrate
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#Reshape input data to 784 = 28 * 28 (image data is 28x28)
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
@@ -39,28 +52,24 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 #Build neural network model. 
 model = Sequential()
-model.add(Dense(800, input_shape=(784,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(400))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(100))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
+model.add(LSTM(50, input_shape=(28,28)))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
 model.summary()
 
-sgd = SGD(lr=0.1, decay=0.0)
+sgd = SGD(lr=0.0)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
+#Callbacks to run after each epoch
+temp_history = LossHistory()
+lr = LearningRateScheduler(step_decay)
+
 history = model.fit(X_train, Y_train,
                     batch_size=batch_size, nb_epoch=nb_epoch,
-                    verbose=1, validation_data=(X_test, Y_test))
+                    verbose=1, validation_data=(X_test, Y_test), callbacks=[lr, temp_history])
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
