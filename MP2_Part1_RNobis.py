1602af2aa1982512fb198917759ea64a5ec4c01b
Ryan Nobis <rcnobis@gmail.com>
2017-02-26 19:16:48 -0500

    Fixed the input shape for Part1.

--- MP2_Part1_RNobis.py
+++ MP2_Part1_RNobis.py
@@ -26,8 +26,8 @@ nb_epoch = 50
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 #Reshape input data to 784 pointwise
-X_train = X_train.reshape(60000, 1, 784)
-X_test = X_test.reshape(10000, 1, 784)
+X_train = X_train.reshape(60000, 784, 1)
+X_test = X_test.reshape(10000, 784, 1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
@@ -41,7 +41,7 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 #Build neural network model. 
 model = Sequential()
-model.add(LSTM(100, input_shape=(1,784)))
+model.add(LSTM(100, input_shape=(784, 1)))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
