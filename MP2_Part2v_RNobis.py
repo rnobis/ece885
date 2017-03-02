2d84f6cd5bf3efd9efda9e0257770af296517026
Ryan Nobis <rcnobis@gmail.com>
2017-03-02 11:28:42 -0500

    Added files for more jobs on HPCC. Updated MP2 Part2 and Part2v to have 100 epochs.

--- MP2_Part2v_RNobis.py
+++ MP2_Part2v_RNobis.py
@@ -19,7 +19,7 @@ from keras.utils import np_utils
 
 batch_size = 32 #128
 nb_classes = 10
-nb_epoch = 50
+nb_epoch = 100
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
