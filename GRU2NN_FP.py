0db76396a202b9fb022fac2c71134aecfd01a738
Ryan Nobis <rcnobis@gmail.com>
2017-04-20 11:06:11 -0400

    Updated learning rate and NN structure.

--- GRU2NN_FP.py
+++ GRU2NN_FP.py
@@ -61,14 +61,16 @@ X_test = X_test.reshape(X_test.shape[0],1,X_test.shape[1])
                                             
 #Build neural network model. 
 model = Sequential()
-model.add(GRU2(10, input_shape=(1,53), consume_less='mem'))
+model.add(GRU2(25, input_shape=(1,53), consume_less='mem'))
+model.add(Dropout(0.2))
+model.add(Dense(10))
 model.add(Dropout(0.2))
 model.add(Dense(1))
 #model.add(Activation('softmax'))
 
 model.summary()
 
-sgd = SGD(lr=0.01, decay=0.0, momentum=0.01)
+sgd = SGD(lr=0.1, decay=0.0, momentum=0.01)
 model.compile(loss='mean_squared_error',
               optimizer=sgd,
               metrics=['accuracy'])
