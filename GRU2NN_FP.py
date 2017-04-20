9e87156d0ab265cf8da61ada044c0c89071fc2f5
Ryan Nobis <rcnobis@gmail.com>
2017-04-20 10:09:06 -0400

    Updated learning rate for GRU2NN_FP.py

--- GRU2NN_FP.py
+++ GRU2NN_FP.py
@@ -65,7 +65,7 @@ model.add(Activation('softmax'))
 
 model.summary()
 
-sgd = SGD(lr=1e-3, decay=0.0)
+sgd = SGD(lr=1e-2, decay=0.0)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
