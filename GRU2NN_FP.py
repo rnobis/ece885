5499e1c585aa92aac078c9b9dffab6a82797c885
Ryan Nobis <rcnobis@gmail.com>
2017-04-25 09:01:35 -0400

    Updated GRU2NN_FP to run on each variant.

--- GRU2NN_FP.py
+++ GRU2NN_FP.py
@@ -19,116 +19,124 @@ batch_size = 1
 nb_classes = 10
 nb_epoch = 5
 
-#import training data
-trainData = np.genfromtxt('Features_Variant_1.csv', delimiter = ",")
-#trainData = np.genfromtxt('Features_Variant_2.csv', delimiter = ",")
-#trainData = np.genfromtxt('Features_Variant_3.csv', delimiter = ",")
-#trainData = np.genfromtxt('Features_Variant_4.csv', delimiter = ",")
-#trainData = np.genfromtxt('Features_Variant_5.csv', delimiter = ",")
-X_train = trainData[:,0:53]
-y_train = trainData[:,53]
+#Run RNN on each training dataset
+for j in range (1, 5):
+    #import training data
+    if j == 1:
+        trainData = np.genfromtxt('Features_Variant_1.csv', delimiter = ",")
+    elif j == 2:
+        trainData = np.genfromtxt('Features_Variant_2.csv', delimiter = ",")
+    elif j == 3:
+        trainData = np.genfromtxt('Features_Variant_3.csv', delimiter = ",")
+    elif j == 4:
+        trainData = np.genfromtxt('Features_Variant_4.csv', delimiter = ",")
+    elif j == 5:
+        trainData = np.genfromtxt('Features_Variant_5.csv', delimiter = ",")
+        
+    X_train = trainData[:,0:53]
+    y_train = trainData[:,53]
 
-#randomly select test data to import
-rtd = np.random.random_integers(1,high=10)
+    #randomly select test data to import
+    rtd = np.random.random_integers(1,high=10)
 
-if rtd == 1:
-    testData = np.genfromtxt('Test_Case_1.csv', delimiter = ",")
-elif rtd == 2:
-    testData = np.genfromtxt('Test_Case_2.csv', delimiter = ",")
-elif rtd == 3:
-    testData = np.genfromtxt('Test_Case_3.csv', delimiter = ",")
-elif rtd == 4:
-    testData = np.genfromtxt('Test_Case_4.csv', delimiter = ",")
-elif rtd == 5:
-    testData = np.genfromtxt('Test_Case_5.csv', delimiter = ",")
-elif rtd == 6:
-    testData = np.genfromtxt('Test_Case_6.csv', delimiter = ",")
-elif rtd == 7:
-    testData = np.genfromtxt('Test_Case_7.csv', delimiter = ",")
-elif rtd == 8:
-    testData = np.genfromtxt('Test_Case_8.csv', delimiter = ",")
-elif rtd == 9:
-    testData = np.genfromtxt('Test_Case_9.csv', delimiter = ",")
-elif rtd == 10:
-    testData = np.genfromtxt('Test_Case_10.csv', delimiter = ",")
+    if rtd == 1:
+        testData = np.genfromtxt('Test_Case_1.csv', delimiter = ",")
+    elif rtd == 2:
+        testData = np.genfromtxt('Test_Case_2.csv', delimiter = ",")
+    elif rtd == 3:
+        testData = np.genfromtxt('Test_Case_3.csv', delimiter = ",")
+    elif rtd == 4:
+        testData = np.genfromtxt('Test_Case_4.csv', delimiter = ",")
+    elif rtd == 5:
+        testData = np.genfromtxt('Test_Case_5.csv', delimiter = ",")
+    elif rtd == 6:
+        testData = np.genfromtxt('Test_Case_6.csv', delimiter = ",")
+    elif rtd == 7:
+        testData = np.genfromtxt('Test_Case_7.csv', delimiter = ",")
+    elif rtd == 8:
+        testData = np.genfromtxt('Test_Case_8.csv', delimiter = ",")
+    elif rtd == 9:
+        testData = np.genfromtxt('Test_Case_9.csv', delimiter = ",")       
+    elif rtd == 10:
+        testData = np.genfromtxt('Test_Case_10.csv', delimiter = ",")
     
-X_test = testData[:,0:53]
-y_test = testData[:,53]
+    X_test = testData[:,0:53]
+    y_test = testData[:,53]
 
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
 
-#Reclassify Output Data to range of comments
-i = 0
-for i in range (0, y_train.shape[0]):
-    if y_train[i] == 0:
-        y_train[i] = 1
-    elif y_train[i] > 0 and y_train[i] < 5:
-        y_train[i] = 2
-    elif y_train[i] >= 5 and y_train[i] < 10:
-        y_train[i] = 3
-    elif y_train[i] >= 10 and y_train[i] < 50:
-        y_train[i] = 4
-    elif y_train[i] >= 50 and y_train[i] < 100:
-        y_train[i] = 5
-    elif y_train[i] >= 100 and y_train[i] < 500:
-        y_train[i] = 6
-    elif y_train[i] >= 500 and y_train[i] < 1000:
-        y_train[i] = 7
-    elif y_train[i] >= 1000 and y_train[i] < 5000:
-        y_train[i] = 8
-    elif y_train[i] >= 5000 and y_train[i] < 10000:
-        y_train[i] = 9
-    elif y_train[i] >= 10000:
-        y_train[i] = 10
+    #Reclassify Output Data to range of comments
+    i = 0
+    for i in range (0, y_train.shape[0]):
+        if y_train[i] == 0:
+            y_train[i] = 1
+        elif y_train[i] > 0 and y_train[i] < 5:
+            y_train[i] = 2
+        elif y_train[i] >= 5 and y_train[i] < 10:
+            y_train[i] = 3
+        elif y_train[i] >= 10 and y_train[i] < 50:
+            y_train[i] = 4
+        elif y_train[i] >= 50 and y_train[i] < 100:
+            y_train[i] = 5
+        elif y_train[i] >= 100 and y_train[i] < 500:
+            y_train[i] = 6
+        elif y_train[i] >= 500 and y_train[i] < 1000:
+            y_train[i] = 7
+        elif y_train[i] >= 1000 and y_train[i] < 5000:
+            y_train[i] = 8
+        elif y_train[i] >= 5000 and y_train[i] < 10000:
+            y_train[i] = 9
+        elif y_train[i] >= 10000:
+            y_train[i] = 10
                
-for i in range (0, y_test.shape[0]):
-    if y_test[i] == 0:
-        y_test[i] = 1
-    elif y_test[i] > 0 and y_test[i] < 5:
-        y_test[i] = 2
-    elif y_test[i] >= 5 and y_test[i] < 10:
-        y_test[i] = 3
-    elif y_test[i] >= 10 and y_test[i] < 50:
-        y_test[i] = 4
-    elif y_test[i] >= 50 and y_test[i] < 100:
-        y_test[i] = 5
-    elif y_test[i] >= 100 and y_test[i] < 500:
-        y_test[i] = 6
-    elif y_test[i] >= 500 and y_test[i] < 1000:
-        y_test[i] = 7
-    elif y_test[i] >= 1000 and y_test[i] < 5000:
-        y_test[i] = 8
-    elif y_test[i] >= 5000 and y_test[i] < 10000:
-        y_test[i] = 9
-    elif y_test[i] >= 10000:
-        y_test[i] = 10
+    for i in range (0, y_test.shape[0]):
+        if y_test[i] == 0:
+            y_test[i] = 1
+        elif y_test[i] > 0 and y_test[i] < 5:
+            y_test[i] = 2
+        elif y_test[i] >= 5 and y_test[i] < 10:
+            y_test[i] = 3
+        elif y_test[i] >= 10 and y_test[i] < 50:
+            y_test[i] = 4
+        elif y_test[i] >= 50 and y_test[i] < 100:
+            y_test[i] = 5
+        elif y_test[i] >= 100 and y_test[i] < 500:
+            y_test[i] = 6
+        elif y_test[i] >= 500 and y_test[i] < 1000:
+            y_test[i] = 7
+        elif y_test[i] >= 1000 and y_test[i] < 5000:
+            y_test[i] = 8
+        elif y_test[i] >= 5000 and y_test[i] < 10000:
+            y_test[i] = 9
+        elif y_test[i] >= 10000:
+            y_test[i] = 10
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-#Reshape data to 3 dimensions (time step = 1)
-X_train = X_train.reshape(X_train.shape[0],1,X_train.shape[1])
-X_test = X_test.reshape(X_test.shape[0],1,X_test.shape[1])
+    #Reshape data to 3 dimensions (time step = 1)
+    X_train = X_train.reshape(X_train.shape[0],1,X_train.shape[1])
+    X_test = X_test.reshape(X_test.shape[0],1,X_test.shape[1])
                                             
-#Build neural network model. 
-model = Sequential()
-model.add(GRU2(25, input_shape=(1,53), consume_less='mem'))
-model.add(Dropout(0.2))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
+    #Build neural network model. 
+    model = Sequential()
+    model.add(GRU2(25, input_shape=(1,53), consume_less='mem'))
+    model.add(Dropout(0.2))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    
+    model.summary()
 
-model.summary()
+    sgd = SGD(lr=0.0001, decay=0.0)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=sgd,
+                  metrics=['accuracy'])
 
-sgd = SGD(lr=0.0001, decay=0.0)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
-
-history = model.fit(X_train, Y_train,
-                    batch_size=batch_size, nb_epoch=nb_epoch,
-                    verbose=1, validation_data=(X_test, Y_test))
-score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+    history = model.fit(X_train, Y_train,
+                        batch_size=batch_size, nb_epoch=nb_epoch,
+                        verbose=1, validation_data=(X_test, Y_test))
+    score = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
