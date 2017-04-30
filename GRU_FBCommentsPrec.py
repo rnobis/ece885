33825e88fcf6dea82f74dd3e77beeb1bce279661
Ryan Nobis <rcnobis@gmail.com>
2017-04-30 17:12:58 -0400

    Added second GRU layer.

--- GRU_FBCommentsPrec.py
+++ GRU_FBCommentsPrec.py
@@ -76,7 +76,7 @@ for j in range (0, 5):
     #Build neural network model. 
     model = Sequential()
     model.add(GRU(20, input_shape=(1,53), consume_less='mem'))
-    #model.add(Dropout(0.2))
+    model.add(GRU(10))
     model.add(Dense(1))
     
     model.summary()
