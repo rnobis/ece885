2a76fe2d5698d11ab529200d38ac9ffcd7adbddf
Ryan Nobis <rcnobis@gmail.com>
2017-04-30 22:02:26 -0400

    Created folders for MP1 and MP2. Reorganized files.

---
+++ MP2/GRU_MNIST.py
@@ -0,0 +1,53 @@
+'''GRU Test
+Trains a GRU RNN on the MNIST dataset using 
+row wise sequence generation for the input.
+
+Gets to XX.XX% test accuracy after 100 epochs
+'''
+
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+#from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.optimizers import SGD
+from keras.layers import GRU
+from keras.utils import np_utils
+
+batch_size = 32 
+nb_classes = 10
+nb_epoch = 100
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+#Build neural network model. 
+model = Sequential()
+model.add(GRU(50, input_shape=(28,28),consume_less='mem'))
+model.add(Dropout(0.2))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.summary()
+
+sgd = SGD(lr=1e-3, decay=0.0)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+history = model.fit(X_train, Y_train,
+                    batch_size=batch_size, nb_epoch=nb_epoch,
+                    verbose=1, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
